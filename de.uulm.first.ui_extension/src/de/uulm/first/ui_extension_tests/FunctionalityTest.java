0690ffeeba3ce33f3e7b1f4d876dcef31036926f
eric <fleri94@web.de>
2020-11-29 13:24:34 +0100

    travis config and Test

---
+++ de.uulm.first.ui_extension/src/de/uulm/first/ui_extension_tests/FunctionalityTest.java
@@ -0,0 +1,14 @@
+package de.uulm.first.ui_extension_tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FunctionalityTest {
+
+	@Test
+	public void test() {
+		assertEquals(42, 40+2);
+	}
+
+}
