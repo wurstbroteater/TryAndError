ea4ac5efc6e1f10b806511fae0b473b731ecc4f8
eric <fleri94@web.de>
2020-12-01 15:27:02 +0100

    fixed tests and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TryAndError
 Test eclipse plugin development and Travis
+
+## Requirements
+The default java version (in linux the path of $JAVA_HOME) needs to be set to java 1.8 (no 11 or higher) due to OSGI problems
+
--- de.uulm.first.ui_extension-test/test/de/uulm/first/ui_extension/FunctionalityTest.java
+++ de.uulm.first.ui_extension-test/test/de/uulm/first/ui_extension/FunctionalityTest.java
@@ -7,8 +7,19 @@ import org.junit.Test;
 public class FunctionalityTest {
 
 	@Test
-	public void test() {
+	public void meaningOfEverythingTest() {
 		assertEquals(42, 40 + 2);
 	}
+	
+	@Test
+	public void aSecondTest() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void aFailedTest() {
+		assertTrue(!false);
+	}
+	
 
 }
